941a7e907fdc0ba0dfb2723b4bbd0f0964f40ac7
alejandrolozano13 <alejandro20181999@gmail.com>
2023-11-19 13:26:22 -0300

    comeco do projeto

---
+++ LaLigaProject/Jogador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaLigaProject
+{
+    public class Jogador
+    {
+        public int Id { get; set; }
+        public int TimeId { get; set; }
+        public string Nome { get; set; }
+        public string Posicao { get; set; }
+        public string Nacionalidade { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public enum Sexo
+        {
+            Masculino,
+            Feminino
+        }
+        public Sexo Genero { get; set; }
+        public string? FotoDoJogador { get; set; }
+    }
+}
---
+++ LaLigaProject/Time.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaLigaProject
+{
+    public class Time
+    {
+        public int Id { get; set; }
+        public string NomeDoTime { get; set; }
+        public string PaisDeOrigem { get; set; }
+        public string CampoSede { get; set; }
+        public DateTime AnoDeCriacao { get; set; }
+        public List <Jogador> Jogadores { get; set; }
+    }
+}
